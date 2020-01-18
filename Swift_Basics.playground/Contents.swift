f03de3b33fb50dc49d77bc877f96ae43180ccc72
Anthony Zhang <zhangchongguang@bilibili.com>
2020-01-18 15:30:37 +0800

    swift basic

--- Swift_Basics.playground/Contents.swift
+++ Swift_Basics.playground/Contents.swift
@@ -1,3 +1,23 @@
 import UIKit
 
-var str = "Hello, playground"
+let str = "Hello, playground"
+print(str)
+
+var friendlyWelcome = "Hello!"
+friendlyWelcome = "Bonjour!"
+// friendlyWelcome的值现在变为Bonjour
+
+var intType: Int = -100
+var uintType: UInt = 100
+var floatType: Float = 100.5
+var doubleType: Double = 100.5
+var stringType: String = "hello"
+var boolType: Bool = true
+var tupleType: (Int,String) = (1,"hello")
+
+var myString = "7"
+var possibleInt: Int? = Int(myString)
+print(possibleInt!)
+// 强制解析possibleInt
+
+
