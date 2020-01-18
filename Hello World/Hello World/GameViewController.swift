e345da5372623cdb9cb8e743586dafd95ce9bee6
Anthony Zhang <zhangchongguang@bilibili.com>
2020-01-18 14:41:45 +0800

    Swift program & playground

---
+++ Hello World/Hello World/GameViewController.swift
@@ -0,0 +1,54 @@
+//
+//  GameViewController.swift
+//  Hello World
+//
+//  Created by Anthony Zhang on 2020/1/18.
+//  Copyright © 2020 Anthony Zhang. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = SKScene(fileNamed: "GameScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+    
+    // 自动旋转
+    override var shouldAutorotate: Bool {
+        return true
+    }
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return .landscape
+    }
+//    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+//        if UIDevice.current.userInterfaceIdiom == .phone {
+//            return .allButUpsideDown
+//        } else {
+//            return .all
+//        }
+//    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
